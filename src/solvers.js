3f660f986b8a3c81b21b264338f217ad5e111005
Hack Reactor Student <student@hackreactor.com>
2017-07-12 21:48:25 -0700

    partial implementation of brute force on queens solution

--- src/solvers.js
+++ src/solvers.js
@@ -16,15 +16,33 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
+  var solution = []; //fixme
+  for (var i = 0; i < n; i++) {
+    solution.push([]);
+  }
+  var col = 0;
+  for (var i = 0; i < n; i++) {
+    solution[i][col] = 1;
+    for (var j = col + 1; j < n; j++) {
+      solution[i][j] = 0;
+    }
+    for (var k = col - 1; k >= 0; k--) {
+      solution[i][k] = 0;
+    }
+    col++;
+  }
+  //takes as input a integer
+  //outputs a matrix with the correct values to represent a solution 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 1;
+  for (var i = 1; i <= n; i++) {
+    solutionCount *= i;
+  }
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
@@ -33,7 +51,88 @@ window.countNRooksSolutions = function(n) {
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
   var solution = undefined; //fixme
+  var makeEmptyMatrix = function(n) {
+    return _(_.range(n)).map(function() {
+      return _(_.range(n)).map(function() {
+        return 0;
+      });
+    });
+  };
+  
+  // makeEmptyMatrix
+  var matrix = makeEmptyMatrix(n);
+  // recursive function
+  var findSolution = function (n, rowIndex, matrix) {
+    // if matrix[rowIndex] > n
+    if (matrix[rowIndex] > n) {
+      // return matrix
+      return matrix;
+    }
+    // if matrix[rowIndex] does not include 0s
+    if (!matrix[rowIndex].includes(0)) {
+      // return
+      return false;
+    }
+    // for each item of matrix[rowIndex]
+    for (var i = 0; i < n; i++) {
+      // if matrix[rowIndex][i] is 0
+      if (matrix[rowIndex][i] === 0) {
+        // place a 2 at matrix[rowIndex][i]
+        matrix[rowIndex][i] = 2;
+        var columnConflict = false;
+        // change 0s in range of 2 to 1s
+        for (var j = 0; j < n; j++) {
+          if (matrix[j][i] === 2) {
+            // stop this possible matrix
+            columnConflict = true;
+            break;
+          } else {
+            matrix[j][i] = 1;
+          }
+          if (matrix[rowIndex][j] !== 2) {
+            matrix[rowIndex][j] = 1;
+          }
+        }
+      
+        }
+        // make left diagonals 1s
+        var rowsFromCurr = 1;
+        for (var k = i - 1; i >= 0; i--) {
+          if (rowIndex - rowsFromCurr >= 0) {
+            if (matrix[rowIndex - rowsFromCurr][k] === 2) {
+              columnConflict = true;
+              break;
+            } else {
+              matrix[rowIndex - rowsFromCurr][k] = 1;
+            }
+          }
+          if (rowIndex + rowsFromCurr < n) {
+            if (matrix[rowIndex + rowsFromCurr][k] === 2) {
+              columnConflict = true;
+              break;
+            } else {
+              matrix[rowIndex + rowsFromCurr][k] = 1;
+            }
+          }
+        }
+        
+        if (columnConflict) {
+          continue;
+        }
+        // recurse on next row
+        var f = findSolution(n, rowIndex + 1, matrix); 
+        if (!f) {
+          continue;
+        } else {
+          return f;
+        }    
+      }
+    }
 
+      
+  };
+  
+  
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
 };
