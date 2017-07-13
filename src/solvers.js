892aab1d660ba7cefd514319120e245c92f7e1eb
Hack Reactor Student <student@hackreactor.com>
2017-07-13 14:46:16 -0700

    Complete FindNQueensSolution

--- src/solvers.js
+++ src/solvers.js
@@ -50,96 +50,120 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
-  var makeEmptyMatrix = function(n) {
-    return _(_.range(n)).map(function() {
-      return _(_.range(n)).map(function() {
-        return 0;
-      });
-    });
-  };
-  
-  // makeEmptyMatrix
-  var matrix = makeEmptyMatrix(n);
+  var result, solution, matrix;
+
   // recursive function
   var findSolution = function (n, rowIndex, matrix) {
     // if matrix[rowIndex] > n
-    if (matrix[rowIndex] > n) {
+    if (rowIndex >= n) {
       // return matrix
       return matrix;
     }
-    // if matrix[rowIndex] does not include 0s
-    if (!matrix[rowIndex].includes(0)) {
-      // return
-      return false;
-    }
-    // for each item of matrix[rowIndex]
+    // for each column in the row
     for (var i = 0; i < n; i++) {
-      // if matrix[rowIndex][i] is 0
-      if (matrix[rowIndex][i] === 0) {
-        // place a 2 at matrix[rowIndex][i]
-        matrix[rowIndex][i] = 2;
-        var columnConflict = false;
-        // change 0s in range of 2 to 1s
-        for (var j = 0; j < n; j++) {
-          if (matrix[j][i] === 2) {
-            // stop this possible matrix
-            columnConflict = true;
-            break;
-          } else {
-            matrix[j][i] = 1;
-          }
-          if (matrix[rowIndex][j] !== 2) {
-            matrix[rowIndex][j] = 1;
-          }
-        }
-      
-        }
-        // make left diagonals 1s
-        var rowsFromCurr = 1;
-        for (var k = i - 1; i >= 0; i--) {
-          if (rowIndex - rowsFromCurr >= 0) {
-            if (matrix[rowIndex - rowsFromCurr][k] === 2) {
-              columnConflict = true;
-              break;
-            } else {
-              matrix[rowIndex - rowsFromCurr][k] = 1;
-            }
-          }
-          if (rowIndex + rowsFromCurr < n) {
-            if (matrix[rowIndex + rowsFromCurr][k] === 2) {
-              columnConflict = true;
-              break;
-            } else {
-              matrix[rowIndex + rowsFromCurr][k] = 1;
-            }
-          }
-        }
-        
-        if (columnConflict) {
-          continue;
-        }
-        // recurse on next row
-        var f = findSolution(n, rowIndex + 1, matrix); 
-        if (!f) {
-          continue;
-        } else {
-          return f;
-        }    
+      //  attempt to put a queen there
+      if (i !== 0) {
+        matrix.togglePiece(rowIndex, i - 1);
       }
-    }
 
-      
+      matrix.togglePiece(rowIndex, i);
+      // if conflict
+      if (!matrix.hasAnyQueensConflicts()) {
+        //remove the queen, and proceed to next column
+        var solution = findSolution(n, rowIndex + 1, matrix); 
+        // matrix.togglePiece(rowIndex, i);
+        if (solution) {
+          return solution;
+        }
+      } 
+      if (i === n - 1) {
+        matrix.togglePiece(rowIndex, i);
+      }
+      // else {
+      //   // if not, call findSolution on next row
+      //   findSolution(n, rowIndex + 1, matrix);  
+      // }
+    }
   };
-  
-  
+  //if N is 0,2, or 3, return dummy input for 0 ==[]
+  if (n === 0 || n === 2 || n === 3) {
+    result = new Board({'n': n});
+    solution = result.rows();
+  } else if (n === 1) {
+    //if N is 1 return [[1]]
+    solution = [[1]];
+  } else {
+    //create matrix of N and run findSolution(n,rowIndex,matrix)
+    matrix = new Board({'n': n});
+    result = findSolution(n, 0, matrix);
+
+    //loop through results rows and push to solution 
+    solution = [];
+    for (var i = 0; i < n; i++) {
+      solution.push(result.get(i));
+    }
+  }
+  //for loop over solution to convert to array of arrays and set to rsutl
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
+ 
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var result, solution, matrix;
+
+  // recursive function
+  var findSolution = function (n, rowIndex, matrix) {
+    // if matrix[rowIndex] > n
+    if (rowIndex >= n) {
+      // return matrix
+      return matrix;
+    }
+    // for each column in the row
+    for (var i = 0; i < n; i++) {
+      //  attempt to put a queen there
+      if (i !== 0) {
+        matrix.togglePiece(rowIndex, i - 1);
+      }
+
+      matrix.togglePiece(rowIndex, i);
+      // if conflict
+      if (!matrix.hasAnyQueensConflicts()) {
+        //remove the queen, and proceed to next column
+        var solution = findSolution(n, rowIndex + 1, matrix); 
+        // matrix.togglePiece(rowIndex, i);
+        if (solution) {
+          return solution;
+        }
+      } 
+      if (i === n - 1) {
+        matrix.togglePiece(rowIndex, i);
+      }
+      // else {
+      //   // if not, call findSolution on next row
+      //   findSolution(n, rowIndex + 1, matrix);  
+      // }
+    }
+  };
+  //if N is 0,2, or 3, return dummy input for 0 ==[]
+  if (n === 0 || n === 2 || n === 3) {
+    result = new Board({'n': n});
+    solution = result.rows();
+  } else if (n === 1) {
+    //if N is 1 return [[1]]
+    solution = [[1]];
+  } else {
+    //create matrix of N and run findSolution(n,rowIndex,matrix)
+    matrix = new Board({'n': n});
+    result = findSolution(n, 0, matrix);
+
+    //loop through results rows and push to solution 
+    solution = [];
+    for (var i = 0; i < n; i++) {
+      solution.push(result.get(i));
+    }
+  }
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
